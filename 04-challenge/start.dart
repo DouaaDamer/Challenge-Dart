0d8cea547d58e0b4abaf73c7ee2fcbb86ab48b63
DouaaDamer <108395517+DouaaDamer@users.noreply.github.com>
2024-05-14 13:34:18 +0300

    Update start.dart

--- 04-challenge/start.dart
+++ 04-challenge/start.dart
@@ -20,4 +20,48 @@ void main() {
 
 
   /// solution
+  import 'dart:io';
+
+void main() {
+  stdout.write('type number first number');
+  String? number1 = stdin.readLineSync();
+  stdout.write('type number secoand number');
+  String? number2 = stdin.readLineSync();
+  stdout.write('enter the opertion');
+  String? opertor = stdin.readLineSync();
+  if ((number1 != null && number1.isNotEmpty) &&
+      (number2 != null && number2.isNotEmpty)) {
+    if (opertor != null && opertor.isNotEmpty) {
+      num? n1 = (int.tryParse(number1)) ?? (double.tryParse(number1));
+      num? n2 = (int.tryParse(number2)) ?? (double.tryParse(number2));
+      if (n1 != null && n2 != null) {
+        num result = calculate(n1, n2, opertor);
+        print('$n1 $opertor $n2 =$result');
+      }
+    } else {
+      print('shoulde type opertor');
+    }
+  } else {
+    print("shaould type number 1 and number2");
+  }
+}
+/// هون طلع عندي مشكله طالب فيها انو ممكن تكون null
+  ///  لما حطيت ؟ 
+  /// راحت بس شفت الفيديو بالحل ما طلع عندك مشاكل وماحطيت انت ؟
+  ///the body might complete normally , causing 'null' to be returned,but the return type ,'num', ia a potentially non-nullable type .try adding eighter a return or a throw statment at the end
+num calculate(num n1, num n2, String op) {
+  switch (op) {
+    case '+':
+      return n1 + n2;
+    case '-':
+      return n1 - n2;
+    case '*':
+      return n1 * n2;
+    case '/':
+      return n1 / n2;
+    case '%':
+      return n1 % n2;
+  }
+}
+
 }
